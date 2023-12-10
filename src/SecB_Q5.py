527c838379ede1b88c97eb0fabfcaf8a42558118
Thomas Breitburd <tmb76@cam.ac.uk>
2023-12-10 16:04:11 +0100

    Added answers to part A, still need to summarise results in an okay manner. But results are very part for now.

--- src/SecB_Q5.py
+++ src/SecB_Q5.py
@@ -1,4 +1,11 @@
-
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import RobustScaler
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
+from sklearn.cluster import SpectralClustering
+from sklearn.cluster import OPTICS
 
 # We use the pre-processing pipeline from the previous question to preprocess the data
 # Load the data
@@ -75,7 +82,9 @@ for pair in high_cor_features:
 # ---------------------------------------------------------------------------------------------
 # (a) Apply 2 different clustering methods
 # ---------------------------------------------------------------------------------------------
-
+print('----------------------------------------------')
+print('----------------- part (a) -------------------')
+print('----------------------------------------------')
 # Get the data without the labels
 Baseline_no_labels = Baseline.drop(columns=['type'])
 
@@ -87,19 +96,48 @@ Baseline_no_labels = pd.DataFrame(scaler.transform(Baseline_no_labels), columns=
 
 # K-means
 
-# Get the inertia for different values of k
-inertia = []
-for k in range(1, 21):
-    kmeans = KMeans(n_clusters=k, random_state=0).fit(Baseline_no_labels)
-    inertia.append(kmeans.inertia_)
+# Get the silhouette scores for different cluster numbers
+silhouette_scores = []
+for k in range(2, 8):
+    Model = KMeans(n_clusters=k,n_init = 10).fit(Baseline_no_labels)
+    cluster_labels = Model.labels_
+    silhouette_scores.append(silhouette_score(Baseline_no_labels, cluster_labels))
 
-# Plot the inertia
+# Plot the silhouette scores
 plt.figure(figsize=(6, 5))
-plt.plot(range(1, 21), inertia, marker='o')
+plt.plot(range(2, 8), silhouette_scores, marker='o')
 plt.grid()
 plt.xlabel('Number of clusters')
-plt.ylabel('Inertia')
-plt.title('Inertia for different cluster numbers')
+plt.ylabel('Silhouette score')
+plt.title('Silhouette score for different cluster numbers')
 plt.tight_layout()
-plt.savefig('Plots/B_Q5a_inertia.png')
+plt.savefig('Plots/B_Q5a_silhouette.png')
 plt.show()
+
+# We choose a cluster number of 3
+optimal_k = 3
+KM_Model = KMeans(n_clusters=3, init ="k-means++", random_state=42).fit(Baseline_no_labels)
+KM_cluster_labels = KM_Model.labels_
+
+# Get some specific K-means cluster outputs
+# Get the cluster centers
+KM_cluster_centers = KM_Model.cluster_centers_
+print('The K-means cluster centers are:')
+print(KM_cluster_centers)
+print('----------------------------------------------')
+
+
+# Get cluster labels and their frequency
+print('The K-means cluster labels and their frequencies are:')
+print(pd.Series(KM_cluster_labels).value_counts())
+print('----------------------------------------------')
+
+# Get the inertia
+print('The K-means inertia is:')
+print(KM_Model.inertia_)
+print('----------------------------------------------')
+
+
+# Spectral clustering
+Spectral_clust = SpectralClustering(n_clusters=3, affinity= 'nearest_neighbors', n_neighbors= 40, random_state=42).fit(Baseline_no_labels)
+
